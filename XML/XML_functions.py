c2f35548bac87194a83188ab7e9ce27223a37ff7
NielsVeenstra <n.veenstra1@student.tue.nl>
2024-03-20 10:45:37 +0100

    Added some comments

--- XML/XML_functions.py
+++ XML/XML_functions.py
@@ -4,6 +4,7 @@ import ast
 
 
 def pretty_print_xml(xml_element, indent="  ", newl="\n", level=0):
+    """Makes the layout of the file readable. Thanx chatGPT"""
     spaces = indent * level
     if len(xml_element):
         if not xml_element.text or not xml_element.text.strip():
@@ -26,24 +27,21 @@ def save_xml(tasks: List[Task], file_name: str = "tasks"):
     dep: dependencies
     """
     root = ET.Element('task_set')
-    for t in tasks:
+    for t in tasks:  # add tasks to tree
         task = ET.SubElement(root, 'task', id=str(t.ID), e=str(t.ExecutionTime), dl=str(t.Deadline),
                              dep=str(t.Dependency))
-        # d = ET.SubElement(task, 'deadline')
-        # d.text = '10'
-
     tree = ET.ElementTree(root)
-    pretty_print_xml(root)
-    tree.write(file_name + '.xml')
+    pretty_print_xml(root)  # do layout magic
+    tree.write(file_name + '.xml')  # Save to the required layout
 
 
 def load_xml(file_path):
     """Loads XML file and stores it to a tasks list"""
-    tree = ET.parse(file_path)
+    tree = ET.parse(file_path)  # loads the file
     root = tree.getroot()
     tasks = []
     i = 0
-    for el in root.findall("task"):
+    for el in root.findall("task"):  # extract the parameters
         t = Task()
         for name, value in el.attrib.items():
             if name == 'id': t.ID = int(value)
@@ -53,4 +51,3 @@ def load_xml(file_path):
         tasks.append(t)
         i += 1
     return tasks
-
