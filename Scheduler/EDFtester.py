e9edf1221428dde8e579daee538652af0a6ea71e
rizeakos <vas.rizeakos@gmail.com>
2024-03-28 21:37:01 +0100

    EDF sims

--- Scheduler/EDFtester.py
+++ Scheduler/EDFtester.py
@@ -1,6 +1,7 @@
 import EDFscheduler
 import os
 import time
+import pandas as pd
 
 def write_list(a_list, write_loc):
     file = open(write_loc,'w')
@@ -9,20 +10,38 @@ def write_list(a_list, write_loc):
     file.close()
 
 def main():
+    ## Select one of the following i:
+    # i=0   #For Vasilis
+    # i=1   #For Daniil
+    # i=2   #For Corniels
+    # i=3   #For Niels    
     data_directory = os.getcwd() + r"\Tasksets"
     counter = 0
     total_files = 0
+    if os.path.exists(os.path.join(data_directory, "EDF_results_" + str(i+1) + ".csv")):
+        df = pd.read_csv(os.path.join(data_directory, "EDF_results_" + str(i+1) + ".csv"))
+    else:
+        df= pd.DataFrame({"name": [], "schedulable": [], "time":[]})
+        df.to_csv(os.path.join(data_directory, "EDF_results_" + str(i+1) + ".csv"), index=False)
+    
+    folder_list = []
     for subdir, dirs, files in os.walk(data_directory):
-        total_files +=1
-        print(subdir)
-        file_loc = []
-        for file in files:
-            file_loc.append(os.path.join(subdir, file))
-        if len(file_loc) == 2:
+        folder_list.append(subdir)
+    file_loc = ["taskset.xml", "dependencies.xml"]
+    for subdir in folder_list[i*271:(i+1)*271]:
+    # for subdir in folder_list[1:-1]:
+        name = os.path.basename(os.path.normpath(subdir))
+        if name == "Tasksets": continue
+        print(name)
+        if not df.isin([name]).any().any():
+            total_files +=1
             start = time.time()
-            schedule = EDFscheduler.EDF(file_loc[1], file_loc[0]) # 1: taskset, 0: dependencies
+            schedule = EDFscheduler.EDF(os.path.join(subdir, file_loc[0]), os.path.join(subdir, file_loc[1])) # 0: taskset, 1: dependencies
             end = time.time()
-            write_list([str(not(not schedule)), end - start], subdir + r'\output.txt')
+            temp = {"name": [name], "schedulable": [not(not(schedule))], 'time':[end-start]}
+            df_temp = pd.DataFrame(temp)
+            df_temp.to_csv(os.path.join(data_directory, "EDF_results_" + str(i+1) + ".csv"), mode='a', index=False, header=False)
+            df = pd.concat([df, df_temp], ignore_index=True)
             if schedule != None: counter +=1
         
     return float(counter), float(total_files)
