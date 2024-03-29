c58c44e2d9d3f592f137389ca5cc8fad9694545b
rizeakos <vas.rizeakos@gmail.com>
2024-03-29 07:38:17 +0100

    EDF

--- Scheduler/EDFtester.py
+++ Scheduler/EDFtester.py
@@ -14,22 +14,22 @@ def main():
     # i=0   #For Vasilis
     # i=1   #For Daniil
     # i=2   #For Corniels
-    i=3   #For Niels    
+    # i=3   #For Niels    
     data_directory = os.getcwd() + r"\Tasksets"
     counter = 0
     total_files = 0
-    if os.path.exists(os.path.join(data_directory, "EDF_results_" + str(i+1) + ".csv")):
-        df = pd.read_csv(os.path.join(data_directory, "EDF_results_" + str(i+1) + ".csv"))
+    if os.path.exists(os.path.join(data_directory, "EDF_results.csv")):
+        df = pd.read_csv(os.path.join(data_directory, "EDF_results.csv"))
     else:
         df= pd.DataFrame({"name": [], "schedulable": [], "time":[]})
-        df.to_csv(os.path.join(data_directory, "EDF_results_" + str(i+1) + ".csv"), index=False)
+        df.to_csv(os.path.join(data_directory, "EDF_results.csv"), index=False)
     
     folder_list = []
     for subdir, dirs, files in os.walk(data_directory):
         folder_list.append(subdir)
     file_loc = ["taskset.xml", "dependencies.xml"]
-    for subdir in folder_list[i*271:(i+1)*271]:
-    # for subdir in folder_list[1:-1]:
+    # for subdir in folder_list[i*271:(i+1)*271]:
+    for subdir in folder_list[1:-1]:
         name = os.path.basename(os.path.normpath(subdir))
         if name == "Tasksets": continue
         print(name)
@@ -40,7 +40,7 @@ def main():
             end = time.time()
             temp = {"name": [name], "schedulable": [not(not(schedule))], 'time':[end-start]}
             df_temp = pd.DataFrame(temp)
-            df_temp.to_csv(os.path.join(data_directory, "EDF_results_" + str(i+1) + ".csv"), mode='a', index=False, header=False)
+            df_temp.to_csv(os.path.join(data_directory, "EDF_results.csv"), mode='a', index=False, header=False)
             df = pd.concat([df, df_temp], ignore_index=True)
             if schedule != None: counter +=1
         
