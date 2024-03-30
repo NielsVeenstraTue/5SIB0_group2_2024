8e33c1108e525b382fc5a4b2886ef725b8920857
rizeakos <vas.rizeakos@gmail.com>
2024-03-30 07:38:01 +0100

    statistical tests

--- statistic_tests/getScedulability.py
+++ statistic_tests/getScedulability.py
@@ -17,12 +17,23 @@ def main():
         if name[:11] == "NXT Motion_" and name[12] != "." : name_list.append(ind)
     
     scedulability = [[0,0],[0,0]]
+    makespanDifference = [0, 0, 0]
+    makespan_EDDF = []
+    makespan_ECF = []
     for ind in name_list:
-        if dfEDDF["schedulable"][ind] and dfECF["schedulable"][ind]: scedulability[0][0] += 1
+        if dfEDDF["schedulable"][ind] and dfECF["schedulable"][ind]: 
+            scedulability[0][0] += 1
+            makespan_EDDF.append(dfEDDF["makespan"][ind])
+            makespan_ECF.append(dfECF["makespan"][ind])
+            if dfEDDF["makespan"][ind] < dfECF["makespan"][ind]: makespanDifference[0] += 1
+            elif dfEDDF["makespan"][ind] > dfECF["makespan"][ind]: makespanDifference[1] += 1
+            else: makespanDifference[2] += 1
+                
         elif dfEDDF["schedulable"][ind] and not dfECF["schedulable"][ind]: scedulability[0][1] += 1
         elif not dfEDDF["schedulable"][ind] and dfECF["schedulable"][ind]: scedulability[1][0] += 1
         else: scedulability[1][1] += 1
         
-    print(scedulability)
+    print(scedulability, makespanDifference)
     print(mcnemar(scedulability, exact=False, correction=False))
+    print(stats.ttest_rel(makespan_EDDF, makespan_ECF))
 main()
